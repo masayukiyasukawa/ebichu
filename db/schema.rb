17727755f940b42acba3fd93ab1078ca0ee3ece9
安川正幸 <yasukawamasayuki@yasupennoMacBook-Air.local>
2019-05-01 10:33:06 +0900

    ログインフォームを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_111955) do
+ActiveRecord::Schema.define(version: 2019_05_01_002907) do
 
   create_table "posts", force: :cascade do |t|
     t.string "song"
