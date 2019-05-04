9b218d984dd56d0b152ad97940ef70d0b7326543
安川正幸 <yasukawamasayuki@yasupennoMacBook-Air.local>
2019-05-04 15:47:51 +0900

    ログイン機能を追加、ヘッダー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # get 'users/new'
   root :to => 'posts#index'
   resources :posts
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
