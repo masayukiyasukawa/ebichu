2a0eb082a0ee1ea8fca47e109deb72852370fc86
安川正幸 <yasukawamasayuki@yasupennoMacBook-Air.local>
2019-05-07 21:16:25 +0900

    ユーザ一覧、削除機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_004541) do
+ActiveRecord::Schema.define(version: 2019_05_07_120216) do
 
   create_table "posts", force: :cascade do |t|
     t.string "song"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_05_05_004541) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
