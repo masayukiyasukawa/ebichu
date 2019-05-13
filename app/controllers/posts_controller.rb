119194bd90ac2ae96442cb74c113afa4eeca62f8
安川正幸 <yasukawamasayuki@yasupennoMacBook-Air.local>
2019-05-13 20:51:43 +0900

    userとpostの紐付け、マイページに自分の投稿を表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,16 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
 
   def index
     @posts = Post.all
   end
-  
-  # def show
-  #   @post = Post.new
-  # end
 
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to root_url
     else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_120216) do
+ActiveRecord::Schema.define(version: 2019_05_13_105612) do
 
   create_table "posts", force: :cascade do |t|
     t.string "song"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_05_07_120216) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
