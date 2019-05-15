b674f81fc2149bad2237908bf9e4aab84d525fb7
安川正幸 <yasukawamasayuki@yasupennoMacBook-Air.local>
2019-05-15 20:28:30 +0900

    投稿削除機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def index
     @posts = Post.all
@@ -17,11 +18,21 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def destroy
+    @post.destroy
+    flash[:success] = "Post deleted"
+    redirect_to request.referrer || root_url
+  end
 
   private
 
   def post_params
     params.require(:post).permit(:song,:member,:comment)
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
   end
 end
